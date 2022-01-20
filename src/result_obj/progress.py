96b46af50b9536df9df498790d04548e9d281ff6
Bystroushaak <bystrousak@kitakitsune.org>
2022-01-20 19:07:25 +0100

    Added .update_handler.

--- src/result_obj/progress.py
+++ src/result_obj/progress.py
@@ -5,12 +5,12 @@ class Progress:
     def __init__(self, db=None):
         self._db = db
         self._percent = 0
+        self.update_handler = None
 
         self.pointer = 0
         self.number_of_items = 0
 
         self._create_tables()
-        self.percent = 0
 
     @property
     def percent(self):
@@ -20,6 +20,7 @@ class Progress:
     def percent(self, value):
         self._percent = value
         self._save_to_db()
+        self._call_update_handler()
 
     def increase(self):
         self.pointer += 1
@@ -28,6 +29,7 @@ class Progress:
 
         self._percent = self.pointer / (self.number_of_items / 100)
         self._save_to_db()
+        self._call_update_handler()
 
     def _save_to_db(self):
         if not self._db:
@@ -39,6 +41,10 @@ class Progress:
             (self._percent, self.pointer, self.number_of_items, time.time())
         )
         self._db.commit()
+
+    def _call_update_handler(self):
+        if self.update_handler:
+            self.update_handler(self)
 
     def _create_tables(self):
         cursor = self._db.cursor()
