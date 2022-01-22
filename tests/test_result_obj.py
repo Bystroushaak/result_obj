f7124e34a630445fb494cb5659f48c2dec0a500b
Bystroushaak <bystrousak@kitakitsune.org>
2022-01-22 20:09:50 +0100

    Added tests of the restore point & more tests of the result property.

--- tests/test_result_obj.py
+++ tests/test_result_obj.py
@@ -13,6 +13,42 @@ class Obj:
 
 def test_store_result():
     result_obj = Result(":memory:")
-    result_obj.result = Obj(1)
 
+    result_obj.result = Obj(1)
     assert result_obj.result.get_value() == 1
+
+    result_obj.result = Obj(2)
+    assert result_obj.result.get_value() == 2
+
+    # only one result is kept
+    cursor = result_obj.db.cursor()
+    cursor.execute("SELECT * FROM Result")
+    data = list(cursor.fetchall())
+    assert len(data) == 1
+
+
+def test_empty_result():
+    result_obj = Result(":memory:")
+
+    assert not result_obj.result
+
+
+def test_store_restore_point():
+    result_obj = Result(":memory:")
+
+    result_obj.restore_point = Obj(1)
+    assert result_obj.restore_point.get_value() == 1
+
+    result_obj.restore_point = Obj(2)
+    assert result_obj.restore_point.get_value() == 2
+
+    cursor = result_obj.db.cursor()
+    cursor.execute("SELECT * FROM RestorePoint")
+    data = list(cursor.fetchall())
+    assert len(data) == 2
+
+
+def test_empty_restore_point():
+    result_obj = Result(":memory:")
+
+    assert not result_obj.restore_point
